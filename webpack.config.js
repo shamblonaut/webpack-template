40567e9abfc532e12619176b842955ef80fe56c5
Shamblonaut <shaheembn@gmail.com>
2024-03-30 15:20:22 +0530

    Setup webpack project

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    
+    module: {
+        rules: [
+            {
+                test: /\.(html)$/,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/html/index.html",
+            filename: "index.html",
+            inject: "body",
+        }),
+    ],
+
+    mode: "development",
+    devtool: "inline-source-map",
+};
